802b01083aba64fb3c0d0b399a9b38c587759267
Tharindu Suraj Chathuranga <suraj.tharindu@gmail.com>
2020-11-02 18:53:49 +0530

    initial commit

---
+++ display_oled.ino
@@ -0,0 +1,165 @@
+
+bool flash_text = 0;
+
+///rhythm, read, rush, release 
+void display_time(){
+  int digit1;
+  int digit2;
+  
+  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+
+  if (mode == 1){
+    if (flash_text == 0){
+      display.setCursor(5, 0);
+      display.println("   ");
+      flash_text = 1;
+      display.setCursor(5, 16);
+      display.println(" R2");
+      display.setCursor(5, 32);
+      display.println(" R3");
+      display.setCursor(5, 48);
+      display.println(" R4");
+    }
+    else {
+      display_all_headings();
+      flash_text = 0;
+    }
+  }
+
+  else if (mode == 2){
+    if (flash_text == 0){
+      display.setCursor(5, 16);
+      display.println("   ");
+      flash_text = 1;
+      display.setCursor(5, 0);
+      display.println(" R1");
+      display.setCursor(5, 32);
+      display.println(" R3");
+      display.setCursor(5, 48);
+      display.println(" R4");
+    }
+    else {
+      display_all_headings();
+      flash_text = 0;
+    }
+  }
+
+  else if (mode == 3){
+    if (flash_text == 0){
+      display.setCursor(5, 32);
+      display.println("   ");
+      flash_text = 1;
+      display.setCursor(5, 0);
+      display.println(" R1");
+      display.setCursor(5, 16);
+      display.println(" R2");
+      display.setCursor(5, 48);
+      display.println(" R4");
+    }
+    else {
+      display_all_headings();
+      flash_text = 0;
+    }
+  }
+
+  else if (mode == 4){
+    if (flash_text == 0){
+      display.setCursor(5, 48);
+      display.println("   ");
+      flash_text = 1;
+      display.setCursor(5, 0);
+      display.println(" R1");
+      display.setCursor(5, 16);
+      display.println(" R2");
+      display.setCursor(5, 32);
+      display.println(" R3");
+      
+    }
+    else {
+      display_all_headings();
+      flash_text = 0;
+    }
+  }
+
+  else {
+    display_all_headings();
+  }
+
+  display.setTextColor(SSD1306_WHITE);
+
+  digit1 = int(timeRhythm);
+  digit2 = int(timeRhythm*10)%10;
+
+  display.setCursor(50, 0);
+  display.println("- ");  
+  display.setCursor(68, 0);
+  display.println(digit1);
+  display.setCursor(78, 0);
+  display.println(".");
+  display.setCursor(90, 0);
+  display.println(digit2);
+
+  digit1 = int(timeRead);
+  digit2 = int(timeRead*10)%10;
+
+  display.setCursor(50, 16);
+  display.println("- ");  
+  display.setCursor(68, 16);
+  display.println(digit1);
+  display.setCursor(78, 16);
+  display.println(".");
+  display.setCursor(90, 16);
+  display.println(digit2);
+
+  digit1 = int(timeRush);
+  digit2 = int(timeRush*10)%10;
+
+  display.setCursor(50, 32);
+  display.println("- ");  
+  display.setCursor(68, 32);
+  display.println(digit1);
+  display.setCursor(78, 32);
+  display.println(".");
+  display.setCursor(90, 32);
+  display.println(digit2);
+  
+  digit1 = int(timeRelease);
+  digit2 = int(timeRelease*10)%10;
+
+  display.setCursor(50, 48);
+  display.println("- ");  
+  display.setCursor(68, 48);
+  display.println(digit1);
+  display.setCursor(78, 48);
+  display.println(".");
+  display.setCursor(90, 48);
+  display.println(digit2);
+
+
+  //delayed start display-------------------------------------------------
+  display.setCursor(115, 48);
+  
+  if (delayed_start == 1){
+    //display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+    display.println("D");
+  }
+
+  else{
+    display.setTextColor(SSD1306_WHITE); // Draw 'inverse' text
+    display.println(" ");
+  }
+
+
+  display.display();
+}
+
+void display_all_headings(){
+    display.setCursor(5, 0);
+    display.println(" R1");
+    display.setCursor(5, 16);
+    display.println(" R2");
+    display.setCursor(5, 32);
+    display.println(" R3");
+    display.setCursor(5, 48);
+    display.println(" R4");
+}
---
+++ read_switch.ino
@@ -0,0 +1,91 @@
+void read_switch(){
+  if (digitalRead(oneBtn) == 0){
+    mode = 1;
+    last_active_time = millis();
+    delay(100);
+  }
+
+  else if (digitalRead(twoBtn) == 0){
+    mode = 2;
+    last_active_time = millis();
+    delay(100);
+  }
+
+  else if (digitalRead(threeBtn) == 0){
+    mode = 3;
+    last_active_time = millis();
+    delay(100);
+  }
+
+  else if (digitalRead(fourBtn) == 0){
+    mode = 4;
+    last_active_time = millis();
+    delay(100);
+  }
+
+  else if (digitalRead(plusBtn) == 0){
+
+    if (mode == 1){
+      timeRhythm = timeRhythm + 0.1;
+      if (timeRhythm > 9.9) timeRhythm = 9.9;
+    }
+
+    else if (mode == 2){
+      timeRead = timeRead + 0.1;
+      if (timeRead > 9.9) timeRead = 9.9;
+    }
+
+    else if (mode == 3){
+      timeRush = timeRush + 0.1;
+      if (timeRush > 9.9) timeRush = 9.9;
+    }
+
+    else if (mode == 4){
+      timeRelease = timeRelease + 0.1;
+      if (timeRelease > 9.9) timeRelease = 9.9;
+    }
+
+    for (int i = 0; i < 10; i++){
+      if (digitalRead(minusBtn) == 0){
+        delayed_start = !delayed_start;
+        delay(500);
+      }
+      
+      delay(8);
+    }
+    last_active_time = millis();
+
+  }
+
+  else if (digitalRead(minusBtn) == 0){
+
+    if (mode == 1){
+      timeRhythm = timeRhythm - 0.1;
+      if (timeRhythm < 0.0) timeRhythm = 0.0;
+    }
+
+    else if (mode == 2){
+      timeRead = timeRead - 0.1;
+      if (timeRead < 0.0) timeRead = 0.0;
+    }
+
+    else if (mode == 3){
+      timeRush = timeRush - 0.1;
+      if (timeRush < 0.0) timeRush = 0.0;
+    }
+
+    else if (mode == 4){
+      timeRelease = timeRelease - 0.1;
+      if (timeRelease < 0.0) timeRelease = 0.0;
+    }
+    delay(100);
+    last_active_time = millis();
+    
+  }
+
+  else if (digitalRead(startBtn) == 0){
+    mode = 0;
+    last_active_time = millis();
+    delay(100);
+  }
+}
